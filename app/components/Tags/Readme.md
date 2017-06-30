e579240bf7e595b4a151bf2ebbadf8fef227121d
Parvez Shaikh <parvez.shaikh@bookmyshow.com>
2017-06-30 16:40:58 +0530

    Tags component updated

---
+++ app/components/Tags/Readme.md
@@ -0,0 +1,7 @@
+### Tags
+
+	<Tag color="#666" fontSize="1rem" border="1px solid #666" textTransform="capitalize">3D</Tag>
+	
+
+
+
--- app/components/Tags/Tags.js
+++ app/components/Tags/Tags.js
@@ -1,39 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import './Header.css';
+import './Tags.css';
 
 /**
  * The only true button.
  */
-export default function Header({ color, fontSize, onClick, children }) {
+export default function Tag({ color, size, onClick, children, background, border, textTransform, fontSize }) {
 	const styles = {
 		color,
 		fontSize,
+		background,
+		border,
+		textTransform,
 	};
 
-	return <h2 className="header" style={styles} onClick={onClick}>{children}</h2>;
+	return <span className="tags" style={styles} onClick={onClick}>{children}</span>;
 }
-Header.propTypes = {
-	/** Header label */
+Tag.propTypes = {
+	/** Tag label */
 	children: PropTypes.string.isRequired,
 	/** The color for the button */
 	color: PropTypes.string,
-	/** The font size of the button */
+	/** The size of the button */
 	fontSize: PropTypes.string,
 	/** Gets called when the user clicks on the button */
 	onClick: PropTypes.func,
+	/** BG COLOR */
+	background: PropTypes.string,
+	border: PropTypes.string,
+	textTransform: PropTypes.string,
 };
-Header.defaultProps = {
+Tag.defaultProps = {
 	color: '#333',
-	fontSize: '1rem',
+	size: 'normal',
+	background: 'transparent',
+	border: 'none',
 	/* eslint-disable no-console */
 	onClick: event => {
 		console.log('You have clicked me!', event.target);
 	},
+	textTransform: 'inherit'
 	/* eslint-enable no-console */
 };
-Header.sizes = {
+Tag.sizes = {
 	small: '10px',
 	normal: '14px',
 	large: '18px',
